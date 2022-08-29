6a242799f627927e7fa2a5cfa0f2fb5a5095ca7d
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-29 18:42:13 -0400

    Create UserSchema.cs

---
+++ schema/UserSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JukaVerse.NET.schema
+{
+    public class User
+    {
+        public static int USER_UID { get; set; }
+        public static string USER_DISPLAY_NAME { get; set; }
+        public static string USERNAME { get; set; }
+        public static string USER_HOST_IP { get; set; }
+
+    }
+}
